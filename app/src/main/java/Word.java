27526214c28469662889fbdf4335c828588b5580
Alexey Sarafannikov <alviksar@gmail.com>
2017-09-26 18:45:46 +0300

    Add Word class

---
+++ app/src/main/java/Word.java
@@ -0,0 +1,21 @@
+/**
+ * Created by sav on 26.09.2017.
+ */
+
+public class Word {
+    private String miwokWord;
+    private String defaultWord;
+
+    public Word(String miwokWord, String defaultWord) {
+        this.miwokWord = miwokWord;
+        this.defaultWord = defaultWord;
+    }
+
+    public String getMiwokWord() {
+        return miwokWord;
+    }
+
+    public String getDefaultWord() {
+        return defaultWord;
+    }
+}
