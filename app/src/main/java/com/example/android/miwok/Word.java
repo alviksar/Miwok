89bb9bd4cfdd2f4da64b38238070f0d08a250aff
Alexey Sarafannikov <alviksar@gmail.com>
2017-09-27 17:00:38 +0300

    Add images to activities

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -5,29 +5,35 @@ package com.example.android.miwok;
  */
 
 public class Word {
+    /**
+     * Constant value that represents no image was provided for this word
+     */
+    private static final int NO_IMAGE_PROVIDED = -1;
     private String mMiwokWord;
     private String mDefaultWord;
-    private int mImageId;
+
+    /**
+     * Image resource ID for the word
+     */
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
 
     /**
      * Create a new Word object.
      *
      * @param defaultWord is the word in a language that the user is already familiar with
-     *                           (such as English)
-     * @param miwokWord is the word in the Miwok language
-     * @param imageId is the drawable resource ID for the image associated with the word
-     *
+     *                    (such as English)
+     * @param miwokWord   is the word in the Miwok language
+     * @param imageId     is the drawable resource ID for the image associated with the word
      */
 
     public Word(String defaultWord, String miwokWord, int imageId) {
         this.mMiwokWord = miwokWord;
         this.mDefaultWord = defaultWord;
-        this.mImageId = imageId;
+        this.mImageResourceId = imageId;
     }
 
-    public Word(String defaultWord, String miwokWord)
-    {
-        this(defaultWord, miwokWord, 0);
+    public Word(String defaultWord, String miwokWord) {
+        this(defaultWord, miwokWord, NO_IMAGE_PROVIDED);
     }
 
     public String getMiwokWord() {
@@ -38,7 +44,14 @@ public class Word {
         return mDefaultWord;
     }
 
-    public int getImageId() {
-        return mImageId;
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
+
+    /**
+     * Returns whether or not there is an image for this word.
+     */
+    public boolean hasImage() {
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -55,13 +55,20 @@ public class WordAdapter extends ArrayAdapter<Word> {
         miwokWordView.setText(currentWord.getMiwokWord());
         defaultWordView.setText(currentWord.getDefaultWord());
 
-        /*
         // Find the ImageView in the list_item.xml layout with the ID list_item_icon
-        ImageView iconView = (ImageView) listItemView.findViewById(R.id.list_item_icon);
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.image4word);
         // Get the image resource ID from the current AndroidFlavor object and
         // set the image to iconView
-        iconView.setImageResource(currentWord.getImageResourceId());
-        */
+        if (currentWord.hasImage()) {
+            // If an image is available, display the provided image based on the resource ID
+            imageView.setImageResource(currentWord.getImageResourceId());
+            // Make sure the view is visible
+            imageView.setVisibility(View.VISIBLE);
+        } else {
+            // Otherwise hide the ImageView (set visibility to GONE)
+            imageView.setVisibility(View.GONE);
+        }
+
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
         return listItemView;
