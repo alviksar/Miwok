77eec87b249db4cf7421672d36c017dca024725e
Alexey Sarafannikov <alviksar@gmail.com>
2017-09-27 16:26:50 +0300

    Change the Word constructor

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,9 +9,15 @@ public class Word {
     private String mDefaultWord;
     private int mImageId;
 
-    public Word(String defaultWord, String miwokWord) {
+    public Word(String defaultWord, String miwokWord)
+    {
+        this(defaultWord, miwokWord, 0);
+    }
+
+    public Word(String defaultWord, String miwokWord, int imageId) {
         this.mMiwokWord = miwokWord;
         this.mDefaultWord = defaultWord;
+        this.mImageId = imageId;
     }
 
     public String getMiwokWord() {
