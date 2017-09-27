ffabb9d3eecc0b478f678b44706473ff9b035ab1
Alexey Sarafannikov <alviksar@gmail.com>
2017-09-27 23:31:49 +0300

    Add background colors

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +14,23 @@ import android.widget.ListAdapter;
 import android.widget.TextView;
 import java.util.List;
 import static android.R.attr.resource;
+import static android.R.attr.theme;
+
 /**
  * Created by sav on 26.09.2017.
  */
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(@NonNull Context context, @NonNull List<Word> words) {
+    private int mBackgroundColor;
+
+    public WordAdapter(@NonNull Context context, @NonNull List<Word> words, int backgroundColor) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);
+        this.mBackgroundColor = backgroundColor;
     }
 
     /**
@@ -43,6 +49,10 @@ public class WordAdapter extends ArrayAdapter<Word> {
         if(listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(
                     R.layout.list_item, parent, false);
+
+            int color = ContextCompat.getColor(getContext(), mBackgroundColor);
+            listItemView.findViewById(R.id.text_layout).setBackgroundColor(color);
+
         }
         // Get the object located at this position in the list
         Word currentWord = getItem(position);
@@ -68,7 +78,14 @@ public class WordAdapter extends ArrayAdapter<Word> {
             // Otherwise hide the ImageView (set visibility to GONE)
             imageView.setVisibility(View.GONE);
         }
-
+/*
+        // Set the theme color for the list item
+        View textContainer = listItemView.findViewById(R.id.text_layout);
+        // Find the color that the resource ID maps to
+        int color = ContextCompat.getColor(getContext(), mBackgroundColor);
+        // Set the background color of the text container View
+        textContainer.setBackgroundColor(color);
+*/
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
         return listItemView;
