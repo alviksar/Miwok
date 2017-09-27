ee17382170bcbf927c8d76acb2838d27f23da13e
Alexey Sarafannikov <alviksar@gmail.com>
2017-09-27 16:31:06 +0300

    Add comment to the Word constructor

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -9,15 +9,25 @@ public class Word {
     private String mDefaultWord;
     private int mImageId;
 
-    public Word(String defaultWord, String miwokWord)
-    {
-        this(defaultWord, miwokWord, 0);
-    }
+    /**
+     * Create a new Word object.
+     *
+     * @param defaultWord is the word in a language that the user is already familiar with
+     *                           (such as English)
+     * @param miwokWord is the word in the Miwok language
+     * @param imageId is the drawable resource ID for the image associated with the word
+     *
+     */
 
     public Word(String defaultWord, String miwokWord, int imageId) {
         this.mMiwokWord = miwokWord;
         this.mDefaultWord = defaultWord;
         this.mImageId = imageId;
+    }
+
+    public Word(String defaultWord, String miwokWord)
+    {
+        this(defaultWord, miwokWord, 0);
     }
 
     public String getMiwokWord() {
