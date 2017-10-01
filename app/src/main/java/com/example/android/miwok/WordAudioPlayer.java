612ae3f73952cea1c12751014def45c3f227cb8a
Alexey Sarafannikov <alviksar@gmail.com>
2017-10-01 20:56:54 +0300

    Add audio focus handing

--- app/src/main/java/com/example/android/miwok/WordAudioPlayer.java
+++ app/src/main/java/com/example/android/miwok/WordAudioPlayer.java
@@ -1,9 +1,8 @@
 package com.example.android.miwok;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-
-import java.io.IOException;
 
 /**
  * Created by sav on 29.09.2017.
@@ -13,7 +12,9 @@ public class WordAudioPlayer {
 
     private static MediaPlayer mMediaPlayer;
 
-    private static  MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
+    private static AudioManager mAudioManager;
+
+    private static MediaPlayer.OnCompletionListener onCompletionListener = new MediaPlayer.OnCompletionListener() {
 
         public void onCompletion(MediaPlayer mediaPlayer) {
             // Toast.makeText(mContext, mWord.getMiwokWord(), Toast.LENGTH_SHORT).show();
@@ -23,16 +24,49 @@ public class WordAudioPlayer {
 
     public static void playWord(Context context, Word word) {
         releaseMediaPlayer();
-        mMediaPlayer = MediaPlayer.create(context, word.getAudioResourceId());
-        mMediaPlayer.start(); // no need to call prepare(); create() does that for you
-        mMediaPlayer.setOnCompletionListener(onCompletionListener);
+        if (mAudioManager == null)
+            mAudioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
+        int result  = mAudioManager.requestAudioFocus(afChangeListener,
+                AudioManager.STREAM_MUSIC, AudioManager.AUDIOFOCUS_GAIN_TRANSIENT);
+        if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+            mMediaPlayer = MediaPlayer.create(context, word.getAudioResourceId());
+            mMediaPlayer.start(); // no need to call prepare(); create() does that for you
+            mMediaPlayer.setOnCompletionListener(onCompletionListener);
+        }
     }
 
     public static void releaseMediaPlayer() {
         if (mMediaPlayer != null) {
             mMediaPlayer.release();
             mMediaPlayer = null;
+            if (mAudioManager != null)
+                mAudioManager.abandonAudioFocus(afChangeListener);
         }
     }
+
+    private static AudioManager.OnAudioFocusChangeListener afChangeListener =
+            new AudioManager.OnAudioFocusChangeListener() {
+                public void onAudioFocusChange(int focusChange) {
+                    if (focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT ||
+                            focusChange == AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK) {
+                        // The AUDIOFOCUS_LOSS_TRANSIENT case means that we've lost audio focus for a
+                        // short amount of time. The AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK case means that
+                        // our app is allowed to continue playing sound but at a lower volume. We'll treat
+                        // both cases the same way because our app is playing short sound files.
+
+                        // Pause playback and reset player to the start of the file. That way, we can
+                        // play the word from the beginning when we resume playback.
+                        mMediaPlayer.pause();
+                        mMediaPlayer.seekTo(0);
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_GAIN) {
+                        // The AUDIOFOCUS_GAIN case means we have regained focus and can resume playback.
+                        mMediaPlayer.start();
+                    } else if (focusChange == AudioManager.AUDIOFOCUS_LOSS) {
+                        // The AUDIOFOCUS_LOSS case means we've lost audio focus and
+                        // Stop playback and clean up resources
+                        releaseMediaPlayer();
+                    }
+                }
+            };
 
 }
