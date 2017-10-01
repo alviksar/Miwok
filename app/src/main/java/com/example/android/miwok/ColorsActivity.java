e507a3eb9963bc423dca8b4814e3ec185ab888f2
Alexey Sarafannikov <alviksar@gmail.com>
2017-10-01 23:13:11 +0300

    Specify the parent activity

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -10,7 +10,9 @@ public class ColorsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState);;
+        // getActionBar().setDisplayHomeAsUpEnabled(true);
+
         setContentView(R.layout.word_list);
 
         final ArrayList<Word> words = new ArrayList<Word>();
