c8bd4ed4f0d0e03653a4ec527037b6f6a1e00dad
Alexey Sarafannikov <alviksar@gmail.com>
2017-10-02 12:52:44 +0300

    Change onCreate in activities

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -6,69 +6,17 @@ import android.widget.ListView;
 
 import java.util.ArrayList;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 public class NumbersActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.word_list);
-
-         // Create a list of words
-        final ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("one", "lutti", R.drawable.number_one, R.raw.number_one));
-        words.add(new Word("two", "otiiko", R.drawable.number_two, R.raw.number_two));
-        words.add(new Word("three", "tolookosu", R.drawable.number_three, R.raw.number_three));
-        words.add(new Word("four", "oyyisa", R.drawable.number_four, R.raw.number_four));
-        words.add(new Word("five", "massokka", R.drawable.number_five, R.raw.number_five));
-        words.add(new Word("six", "temmokka", R.drawable.number_six, R.raw.number_six));
-        words.add(new Word("seven", "kenekaku", R.drawable.number_seven, R.raw.number_seven));
-        words.add(new Word("eight", "kawinta", R.drawable.number_eight, R.raw.number_eight));
-        words.add(new Word("nine", "wo’e", R.drawable.number_nine, R.raw.number_nine));
-        words.add(new Word("ten", "na’aacha", R.drawable.number_ten, R.raw.number_ten));
-
-        // Create an {@link WordAdapter}, whose data source is a list of {@link Word}s. The
-        // adapter knows how to create list items for each item in the list.
-        WordAdapter adapter = new WordAdapter(this, words, R.color.category_numbers);
-
-        // Find the {@link ListView} object in the view hierarchy of the {@link Activity}.
-        // There should be a {@link ListView} with the view ID called list, which is declared in the
-        // word_list.xml file.
-        ListView listView = (ListView) findViewById(R.id.list);
-
-        // Make the {@link ListView} use the {@link WordAdapter} we created above, so that the
-        // {@link ListView} will display list items for each {@link Word} in the list.
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new WordClickListener(NumbersActivity.this));
-
-        /*
-         TextView wordView;
-         LinearLayout rootView = (LinearLayout)findViewById(R.id.simple_list_item_1);
-        for(int i=0; i < words.size(); i++) {
-          //  Log.v("NumbersList", w)
-            wordView = new TextView(this);
-            // Set the text to be word at the current index
-            wordView.setText(words.get(i));
-            // Add this TextView as another child to the root view of this layout
-            rootView.addView(wordView);
-        }
-        */
-
-        /*
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Word word = (Word) adapterView.getItemAtPosition(i);
-                mMediaPlayer = MediaPlayer.create(NumbersActivity.this, word.getAudioResourceId());
-                mMediaPlayer.start(); // no need to call prepare(); create() does that for you
-             //   Toast.makeText(NumbersActivity.this, word.getMiwokWord(),Toast.LENGTH_SHORT).show();
-            }
-        });
-    */
-     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        WordAudioPlayer.releaseMediaPlayer();
+        setContentView(R.layout.activity_category);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.container, new NumbersFragment())
+                .commit();
     }
 }
